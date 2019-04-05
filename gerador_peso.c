9975009ef0ffad86a18384fc3efb291c357fcaf5
Tiago Miguel <tmcstiago@gmail.com>
2019-04-05 08:43:10 -0300

    Ajustando lista de idades

--- gerador_peso.c
+++ gerador_peso.c
@@ -5,11 +5,17 @@
 int main()
 {
   int i;
-  int r = 1000;
+  int r = 18;
   
-  for (i = 0; r <= 50000; i++, r+=100)
+  for (i = 0; r <= 120; i++, r++)
   {
     printf ("%d\n", r);
+    if (r>50) 
+      r+=2;
+    if (r>70)
+      r+=3;
+    if (r>100)
+      r+=3; 
   }
   
   return(0);
